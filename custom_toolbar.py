79cde105f7a20ae7efe8098af0511384818bbfc9
A Boyle <aboyle5@hotmail.com>
2013-06-05 14:01:52 -0700

    added rudimentary data cursor to toolbar

--- custom_toolbar.py
+++ custom_toolbar.py
@@ -1,6 +1,7 @@
 from PyQt4 import QtCore, QtGui
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
-
+from matplotlib import cbook
+import numpy as np
 
 CONFIGURE_SUBPLOTS_BTN_POS = 6
 
@@ -16,6 +17,7 @@ class CustomToolbar(NavigationToolbar):
             if type(child) == QtGui.QToolButton:
                 builtin_buttons.append(child)
 
+        # to remove default button from toolbar...
         #buttons in order, 7 == save 
         #builtin_buttons[7].setParent(None)
 
@@ -28,6 +30,13 @@ class CustomToolbar(NavigationToolbar):
         
         self.grid_cb.stateChanged.connect(self.toggle_grid)
         self.addWidget(self.grid_cb)
+
+        # datacursor toggle button
+        self.datacursor_tb = QtGui.QPushButton("dc")
+        self.datacursor_tb.setCheckable(True)
+        self.datacursor_tb.clicked[bool].connect(self.use_cursors)
+
+        self.addWidget(self.datacursor_tb)
 
     def toggle_grid(self):
         for ax in self.canvas.figure.axes:
@@ -46,3 +55,96 @@ class CustomToolbar(NavigationToolbar):
         # repaint canvas after button release -- this is to
         # make sure the toolbar works properly with animated windows
         self.canvas.draw()
+
+    def use_cursors(self, active):
+        if active:
+            for ax in self.canvas.figure.axes:
+                #self.canvas.mpl_connect('pick_event', DataCursor(ax))
+                for line in ax.lines:
+                    #line.set_picker(5)
+                    pass
+            self.dc = DataCursor([self.canvas.figure.axes[2].lines[0]], template='x: %d\ny: %0.5f')
+        else:
+            while len(self.dc.pick_events) > 0:
+                pe = self.dc.pick_events.pop()
+                self.canvas.mpl_disconnect(pe)
+
+class DataCursor(object):
+    """A simple data cursor widget that displays the x,y location of a
+    matplotlib artist when it is selected."""
+    def __init__(self, artists, tolerance=2, offsets=(-20, 20), 
+                 template='x: %0.2f\ny: %0.2f', display_all=False):
+        """Create the data cursor and connect it to the relevant figure.
+        "artists" is the matplotlib artist or sequence of artists that will be 
+            selected. 
+        "tolerance" is the radius (in points) that the mouse click must be
+            within to select the artist.
+        "offsets" is a tuple of (x,y) offsets in points from the selected
+            point to the displayed annotation box
+        "template" is the format string to be used. Note: For compatibility
+            with older versions of python, this uses the old-style (%) 
+            formatting specification.
+        "display_all" controls whether more than one annotation box will
+            be shown if there are multiple axes.  Only one will be shown
+            per-axis, regardless. 
+        """
+        self.template = template
+        self.offsets = offsets
+        self.display_all = display_all
+        if not cbook.iterable(artists):
+            artists = [artists]
+        self.artists = artists
+        self.axes = tuple(set(art.axes for art in self.artists))
+        self.figures = tuple(set(ax.figure for ax in self.axes))
+
+        self.annotations = {}
+        for ax in self.axes:
+            self.annotations[ax] = self.annotate(ax)
+
+        self.pick_events = []
+        for artist in self.artists:
+            artist.set_picker(tolerance)
+        for fig in self.figures:
+            pe = fig.canvas.mpl_connect('pick_event', self)
+            self.pick_events.append(pe)
+
+    def annotate(self, ax):
+        """Draws and hides the annotation box for the given axis "ax"."""
+        annotation = ax.annotate(self.template, xy=(0, 0), ha='right',
+                xytext=self.offsets, textcoords='offset points', va='bottom',
+                bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
+                arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0')
+                )
+        annotation.set_visible(False)
+        return annotation
+
+    def __call__(self, event):
+        """Intended to be called through "mpl_connect"."""
+        # Rather than trying to interpolate, just display the clicked coords
+        # This will only be called if it's within "tolerance", anyway.
+        xdata, ydata = event.artist.get_data()
+        if len(event.ind) > 1 :
+            # select by closest x value of picked points
+            mx, my = event.mouseevent.xdata, event.mouseevent.ydata
+            x_ind = np.searchsorted(xdata[event.ind], [mx])[0]
+            evt_ind = event.ind[x_ind-1]
+        else:
+            evt_ind = event.ind
+        x, y = xdata[evt_ind], ydata[evt_ind]
+        #x, y = event.mouseevent.xdata, event.mouseevent.ydata
+        annotation = self.annotations[event.artist.axes]
+        if x is not None:
+            if not self.display_all:
+                # Hide any other annotation boxes...
+                for ann in self.annotations.values():
+                    ann.set_visible(False)
+            # Update the annotation in the current axis..
+            annotation.xy = x, y
+            annotation.set_text(self.template % (x, y))
+            annotation.set_visible(True)
+            event.canvas.draw()
+
+def find_closest_index(xdata, ydata, y_loc, x_loc):
+    # given an array and a y value, return x index of closest
+    # array element
+    x_ind = np.searchsorted(xdata, [x_loc])[0]
