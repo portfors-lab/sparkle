248c5c296e49ea514188cee04a5f98b9ab08beba
A Boyle <aboyle5@hotmail.com>
2013-08-13 15:49:26 -0700

    reorganized acquistion testing a little

--- test/integration/test_acq_display_store.py
+++ test/integration/test_acq_display_store.py
@@ -1,8 +1,9 @@
+from audiolab.plotting.chacoplots import LiveWindow
+
 import h5py
 import os
 import time
 import numpy as np
-from PyQt4 import QtCore, QtGui
 
 from PyDAQmx.DAQmxConstants import DAQmx_Val_GroupByScanNumber
 from PyDAQmx.DAQmxTypes import *
@@ -10,6 +11,9 @@ from PyDAQmx.DAQmxTypes import *
 from audiolab.io.daq_tasks import AITaskFinite, AOTaskFinite, AITask, AOTask
 from audiolab.io.structures import BigStore
 from audiolab.plotting.plotz import AnimatedWindow, ScrollingPlot
+from audiolab.calibration.qthreading import TestSignals
+
+from PyQt4.QtGui import QApplication
 
 tempfolder = os.path.join(os.path.abspath(os.path.dirname(__file__)), "tmp")
 
@@ -17,26 +21,31 @@ class TestFileAcquire():
     """
     Test continuous acquisition into a HDF5 file
     """
-
     def setUp(self):
-        print('setup')
         fname = os.path.join(tempfolder,'savetemp.hdf5')
         self.testdata = BigStore(fname, chunksize=2**24)
 
-        npts = 1000
+        npts = 10000
         frequency = 5000
         amp = 2
         x = np.linspace(0,np.pi, npts)
         self.stim = amp * np.sin(frequency*x*2*np.pi)
         self.t = x        
 
-        self.sr = 10000
+        self.sr = 500000
         self.npts = npts
 
-        self.aot = AOTask(b"PCI-6259/ao0", self.sr, self.npts, trigsrc=b"ai/StartTrigger")
+        #keep the generation and acq same duration
+        sr_out = 1000000
+        npts_out = npts/(sr_out/self.sr) 
+
+        self.aot = AOTask(b"PCI-6259/ao0", sr_out, npts_out, 
+                            trigsrc=b"ai/StartTrigger")
         self.ait = AITask(b"PCI-6259/ai0", self.sr, self.npts)
         
         self.aot.write(self.stim)
+
+        self.app = QApplication(['sometext'])
 
     def tearDown(self):
         # delete generated file
@@ -45,6 +54,9 @@ class TestFileAcquire():
         os.remove(fname)
 
     def test_synchronous(self):
+        """
+        Test acquitision, storing data, without plot display
+        """
 
         self.ait.register_callback(self.stashacq,self.npts)
 
@@ -61,15 +73,17 @@ class TestFileAcquire():
 
         assert self.testdata.shape() == (acqtime*self.sr,)
 
-    def test_synch_with_plot(self):
+    def test_synch_with_mpl(self):
+        """
+        Test acquisition, storing data, with matplotlib display
+        """
 
-        app = QtGui.QApplication(['sometext'])
         self.fig = AnimatedWindow(([],[]), ([],[]))
         self.fig.show()
         self.fig.draw_line(0, 0, self.t, self.stim)
         #self.fig.axs[1].set_xlim(0,1)
 
-        self.ait.register_callback(self.stashacq_plot,self.npts)
+        self.ait.register_callback(self.stashacq_mpl, self.npts)
         
         self.aot.start()
         self.ait.start()
@@ -87,7 +101,6 @@ class TestFileAcquire():
         self.fig.close()
 
     def xtest_synch_with_scrollplot(self):
-        app = QtGui.QApplication(['sometext'])
         self.fig = ScrollingPlot(1, 1/self.sr)
         self.fig.show()
 
@@ -108,6 +121,33 @@ class TestFileAcquire():
         
         self.fig.close()
 
+    def test_synch_with_chaco(self):
+
+        self.fig = LiveWindow(2)
+        self.fig.show()
+        self.fig.draw_line(0, 0, self.t, self.stim)
+        QApplication.processEvents()
+
+        self.signals = TestSignals()
+        self.signals.update_data.connect(self.update_display)
+
+        self.ait.register_callback(self.stashacq_chaco,self.npts)
+        
+        self.aot.start()
+        self.ait.start()
+        
+        acqtime = 6 #seconds 
+        #time.sleep(acqtime)
+        raw_input("Press enter to stop")
+
+        self.aot.stop()
+        self.ait.stop()
+        self.testdata.consolidate()
+        self.fig.close()
+        print('no. data points acquired: ', self.testdata.shape(), ' desired ', self.sr*acqtime)
+
+        assert self.testdata.shape() == (acqtime*self.sr,)
+
     def stashacq(self,task):
         try:
             r = c_int32()
@@ -120,7 +160,7 @@ class TestFileAcquire():
             self.ait.stop()
             raise
 
-    def stashacq_plot(self,task):
+    def stashacq_mpl(self,task):
         try:
             r = c_int32()
             inbuffer = np.zeros(task.n)
@@ -148,3 +188,23 @@ class TestFileAcquire():
             self.aot.stop()
             self.ait.stop()
             raise
+
+    def stashacq_chaco(self,task):
+        try:
+            r = c_int32()
+            inbuffer = np.zeros(task.n)
+            task.ReadAnalogF64(task.n,10.0,DAQmx_Val_GroupByScanNumber,inbuffer,
+                               task.n,byref(r),None)
+            self.testdata.append(inbuffer.tolist())
+            
+            self.signals.update_data.emit(self.t, inbuffer)
+            return
+
+        except:
+            self.aot.stop()
+            self.ait.stop()
+            raise
+
+    def update_display(self, xdata, ydata):
+        self.fig.draw_line(1, 0, xdata, ydata)
+        QApplication.processEvents()
