e7274d3f2d9f3eed5bba90649796bf49364eec77
A Boyle <aboyle5@hotmail.com>
2013-10-07 12:41:54 -0700

    better spike detection for single point over threshold

--- tools/spiketools.py
+++ tools/spiketools.py
@@ -4,32 +4,41 @@ from scipy.signal import firwin, lfilter
 def calc_spike_times(signal, threshold, sr, mint=None):
     """ Simple spike dectection, for every continuous set of points over
         given threshold, returns the index of the maximum"""
+    print '-'*20
     times = []
     over, = np.where(signal>threshold)
+    print 'over', over
     segments, = np.where(np.diff(over) > 1)
-    
+    print 'segments before', segments
+
     if len(segments) > 0:
         # add end points to sections for looping
         if segments[0] != 0:
             segments = np.insert(segments, [0], [0])
+        else:
+            #first point in singleton
+            times.append(float(over[0])/sr)
         if segments[-1] != len(over)-1:
             np.insert(segments, [len(segments)], [len(over)-1])
-        segments[0] = segments[0]-1
-        for iseg in range(len(segments)-1):
-            if segments[iseg+1] - segments[iseg] == 1:
+        print 'segments', segments
+
+        for iseg in range(1,len(segments)):
+            if segments[iseg] - segments[iseg-1] == 1:
+                print 'single', iseg
                 # only single point over threshold
                 idx = over[iseg]
             else:
+                segments[0] = segments[0]-1                
+                print 'multiple', iseg
                 # find maximum of continuous set over max
-                idx = over[segments[iseg]+1] + np.argmax(signal[over[segments[iseg]+1]:over[segments[iseg+1]]])
+                idx = over[segments[iseg-1]+1] + np.argmax(signal[over[segments[iseg-1]+1]:over[segments[iseg]]])
             # print 'idx', idx
             times.append(float(idx)/sr)
-    # method borrowed from Lar's analysis code
-    #first run a lowpass filter on signal
+        # check last point for singleton
+        if segments[-1]+2 == len(over):
+            times.append(float(over[-1])/sr)
 
-    # er, this is copied from SE example
-    # h = firwin(numtaps=10, cutoff=5000, nyq=sr/2)
-    # filtered_signal = lfilter(h, 1.0, signal)
+    print 'times', times
     return times
 
 def bin_spikes(spike_times, binsz):
