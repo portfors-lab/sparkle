d03d3e5e0f58ff69efc7254268534f07f99fd4e9
Amy Boyle <aboyle5@hotmail.com>
2015-03-23 18:48:37 -0700

    reload batlab file fix

--- sparkle/data/open.py
+++ sparkle/data/open.py
@@ -5,7 +5,6 @@ def open_acqdata(filename, user='unknown', filemode='w-'):
     if filename.lower().endswith((".hdf5", ".h5")):
         return HDF5Data(filename, user, filemode)
     elif filename.lower().endswith((".pst", ".raw")):
-        filename = filename[:-4]
         return BatlabData(filename, user, filemode)
     else:
         print "File format not supported: ", filename
