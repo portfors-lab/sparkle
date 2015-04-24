4f3d7ff35acb7b17e2c36a4e0f2bc17ece747879
Amy Boyle <aboyle5@hotmail.com>
2015-04-24 11:47:45 -0700

    creating channel picker

---
+++ sparkle/gui/channel_picker.py
@@ -0,0 +1,54 @@
+from QtWrapper import QtCore, QtGui
+
+from sparkle.acq.daq_tasks import get_ai_chans
+
+class ChannelPicker(QtGui.QWidget):
+    def __init__(self, device_name):
+        super(ChannelPicker, self).__init__()
+        
+        layout = QtGui.QGridLayout()
+        cnames = get_ai_chans(device_name)
+
+        layout.addWidget(QtGui.QLabel("Channels for device: "+ device_name), 0, 0)
+        row = 1
+        col = 0
+        max_row = 16
+        self.channel_switches = []
+        for channel in cnames:
+            channel_box = QtGui.QCheckBox(channel)
+            self.channel_switches.append(channel_box)
+            layout.addWidget(channel_box, row, col)
+            row += 1
+            if row > max_row:
+                row = 1
+                col +=1
+
+        okBtn = QtGui.QPushButton("OK")
+        cancelBtn = QtGui.QPushButton("Cancel")
+        layout.addWidget(okBtn, max_row+1, 0)
+        row += 1
+        layout.addWidget(cancelBtn, max_row+1, 1)
+
+        self.setLayout(layout)
+
+    def getSelectedChannels(self):
+        chosen_channel_names = []
+        for channel in self.channel_switches:
+            if channel.isChecked():
+                chosen_channel_names.append(channel.text())
+        return chosen_channel_names
+
+    def setSelectedChannels(self, chosen_channel_names):
+        for channel in self.channel_switches:
+            if channel.text() in chosen_channel_names:
+                channel.setChecked(True)
+            else:
+                channel.setChecked(False)
+
+if __name__ == "__main__":
+    app = QtGui.QApplication([])
+    picker = ChannelPicker("PCI-6259")
+    picker.show()
+    app.exec_()
+
+
